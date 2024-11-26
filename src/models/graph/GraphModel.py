1ac4a1894bdb4f23d98d86873872da52a6049630
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-11-26 16:18:43 +0100

    one more layer

--- src/models/graph/GraphModel.py
+++ src/models/graph/GraphModel.py
@@ -6,9 +6,11 @@ class GraphTokenEncoder(nn.Module):
     def __init__(self, hidden_channels, out_channels):
         super().__init__()
         self.conv1 = SAGEConv((-1, -1), hidden_channels)
-        self.conv2 = SAGEConv((-1, -1), out_channels)
+        self.conv2 = SAGEConv((-1, -1), hidden_channels)
+        self.conv3 = SAGEConv((-1, -1), out_channels)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index).relu()
-        x = self.conv2(x, edge_index)
+        x = self.conv2(x, edge_index).relu()
+        x = self.conv3(x, edge_index)
         return x
