7ca1968ed6b3f539984d9f94e81190a013dde04c
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-12-02 12:50:16 +0100

    more GNNs

--- src/models/graph/GraphModel.py
+++ src/models/graph/GraphModel.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from torch_geometric.nn import SAGEConv, GATConv, GATv2Conv, TransformerConv, GINConv
 
 
-class GraphTokenEncoder(nn.Module):
+class SAGEConvTokenEncoder(nn.Module):
     def __init__(self, hidden_channels, out_channels):
         super().__init__()
         self.conv1 = SAGEConv((-1, -1), hidden_channels)
@@ -14,3 +14,60 @@ class GraphTokenEncoder(nn.Module):
         x = self.conv2(x, edge_index).relu()
         x = self.projection(x)
         return x
+
+
+class GATConvTokenEncoder(nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GATConv((-1, -1), hidden_channels)
+        self.conv2 = GATConv((-1, -1), hidden_channels)
+        self.projection = nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        x = self.projection(x)
+        return x
+
+
+class GATv2ConvTokenEncoder(nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GATv2Conv((-1, -1), hidden_channels)
+        self.conv2 = GATv2Conv((-1, -1), hidden_channels)
+        self.projection = nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        x = self.projection(x)
+        return x
+
+
+class TransformerConvTokenEncoder(nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = TransformerConv((-1, -1), hidden_channels)
+        self.conv2 = TransformerConv((-1, -1), hidden_channels)
+        self.projection = nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        x = self.projection(x)
+        return x
+
+
+class GINConvTokenEncoder(nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GINConv(nn.Linear(-1, hidden_channels))
+        self.conv2 = GINConv(nn.Linear(-1, hidden_channels))
+        self.projection = nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        x = self.projection(x)
+        return x
+    
