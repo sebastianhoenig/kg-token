f02ad799f4d913b745acab038352401b1e4ce1ba
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-12-02 09:06:50 +0100

    proj

--- src/models/graph/GraphModel.py
+++ src/models/graph/GraphModel.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from torch_geometric.nn import SAGEConv
+from torch_geometric.nn import SAGEConv, GATConv, GATv2Conv, TransformerConv, GINConv
 
 
 class GraphTokenEncoder(nn.Module):
@@ -7,10 +7,10 @@ class GraphTokenEncoder(nn.Module):
         super().__init__()
         self.conv1 = SAGEConv((-1, -1), hidden_channels)
         self.conv2 = SAGEConv((-1, -1), hidden_channels)
-        self.conv3 = SAGEConv((-1, -1), out_channels)
+        self.projection = nn.Linear(hidden_channels, out_channels)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index).relu()
         x = self.conv2(x, edge_index).relu()
-        x = self.conv3(x, edge_index)
+        x = self.projection(x)
         return x
