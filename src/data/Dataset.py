f594e0e9ae2e3493aeaa02e1ed11d9bba5063c99
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-12-15 19:27:54 +0100

    no bos/eos

--- src/data/Dataset.py
+++ src/data/Dataset.py
@@ -72,9 +72,9 @@ class GraphQADataset(Dataset):
             EOS_TOKEN = tokenizer.eos_token_id
             PAD_TOKEN = tokenizer.eos_token_id  # TODO CHANGE AS WELL WHEN NO LONGER GPT2
 
-            input_tokens = np.array([BOS_TOKEN] + query_tokens + answer_tokens + [EOS_TOKEN])
+            input_tokens = np.array(query_tokens + answer_tokens)
             target_mask = np.zeros_like(input_tokens)
-            target_mask[len(query_tokens) + 1] = 1  # TRYING THIS OUT - REMOVING EOS TOKEN FROM TARGET MASK
+            target_mask[len(query_tokens)] = 1  # TRYING THIS OUT - REMOVING EOS TOKEN FROM TARGET MASK
             orig_len = len(query_tokens) + len(answer_tokens) + 1
             #input_tokens = np.pad(input_tokens, [[0, max_tokens - orig_len-1]], constant_values=PAD_TOKEN)
             #target_mask = np.pad(target_mask, [[0, max_tokens - orig_len-1]], constant_values=0)
