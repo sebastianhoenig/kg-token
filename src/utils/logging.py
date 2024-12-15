f564d149e79f4f8b67766ad730f20f35e31b72d5
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-12-15 17:34:20 +0100

    logging

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -11,12 +11,14 @@ def log_to_wandb(res, epoch, example_ct, loss, optimizer):
     num_items = res['num_items']
     num_correct = res['num_correct']
     accuracy = num_correct / num_items
+    yes_preds = correct_yes_preds+wrong_yes_preds
+    no_preds = correct_no_preds+wrong_no_preds
 
     wandb.log({"epoch": epoch, "loss": loss}, step=example_ct)
     wandb.log({"epoch": epoch, "accuracy": accuracy}, step=example_ct)
     wandb.log({"epoch": epoch, "learning_rate": optimizer.param_groups[0]['lr']}, step=example_ct)
-    wandb.log({"epoch": epoch, "yes_preds": correct_yes_preds+wrong_yes_preds, "wrong_yes_preds": wrong_yes_preds}, step=example_ct)
-    wandb.log({"epoch": epoch, "no_preds": correct_no_preds+wrong_no_preds, "wrong_no_preds": wrong_no_preds}, step=example_ct)
+    wandb.log({"epoch": epoch, "yes_preds": yes_preds, "wrong_yes_preds": wrong_yes_preds}, step=example_ct)
+    wandb.log({"epoch": epoch, "no_preds": no_preds, "wrong_no_preds": wrong_no_preds}, step=example_ct)
     wandb.log({"epoch": epoch, "yes_targets": yes_targets, "no_targets": no_targets}, step=example_ct)
 
 
@@ -31,13 +33,15 @@ def log_evaluation_to_wandb(res):
     num_correct = res['num_correct']
 
     accuracy = num_correct/num_items if num_items > 0 else 0
+    yes_preds = correct_yes_preds + wrong_yes_preds
+    no_preds = correct_no_preds + wrong_no_preds
 
     data = [
         ["accuracy", accuracy],
-        ["yes_preds", correct_yes_preds+wrong_yes_preds],
+        ["yes_preds", yes_preds],
         ["wrong_yes_preds", wrong_yes_preds],
         ["yes_targets", yes_targets],
-        ["no_preds", correct_no_preds+wrong_no_preds],
+        ["no_preds", no_preds],
         ["wrong_no_preds", wrong_no_preds],
         ["no_targets", no_targets],
         ["num_items", num_items],
