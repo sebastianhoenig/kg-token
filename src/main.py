e3c0429532e8b78d037f5004161c07f25a158a2c
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-12-18 08:08:32 +0100

    max new tokens == 1

--- src/main.py
+++ src/main.py
@@ -72,6 +72,47 @@ def train(config: Any):
     print("Evaluating on Test set")
     save_model(gnn_llm, config)
 
+    test_dataset = GraphQADataset(graph=movielens.test, config=config)
+    test_loader = DataLoader(test_dataset, batch_size=config.batch_size)
+
+    gnn_llm.eval()
+
+    total_correct_yes_preds, total_correct_no_preds = 0, 0
+    total_wrong_yes_preds, total_wrong_no_preds = 0, 0
+    total_yes_targets, total_no_targets = 0, 0
+    total_correct, total_items = 0, 0
+
+    with torch.no_grad():
+        for batch in tqdm(test_loader, desc="Evaluating"):
+            test_results = gnn_llm.inference(batch, graph)
+            print("Predictions")
+            print(test_results["predictions"])
+            print("Answers")
+            print(test_results["answers"])
+            batch_res = yes_no_accuracy(test_results["answers"], test_results["predictions"])
+
+            total_correct_yes_preds += batch_res['num_correct_yes_preds']
+            total_correct_no_preds += batch_res['num_correct_no_preds']
+            total_wrong_yes_preds += batch_res['num_wrong_yes_preds']
+            total_wrong_no_preds += batch_res['num_wrong_no_preds']
+            total_yes_targets += batch_res['num_yes_targets']
+            total_no_targets += batch_res['num_no_targets']
+            total_correct += batch_res['num_correct']
+            total_items += batch_res['num_items']
+
+    aggregated_res = {
+        "num_correct": total_correct,
+        "num_correct_yes_preds": total_correct_yes_preds,
+        "num_correct_no_preds": total_correct_no_preds,
+        "num_wrong_yes_preds": total_wrong_yes_preds,
+        "num_wrong_no_preds": total_wrong_no_preds,
+        "num_yes_targets": total_yes_targets,
+        "num_no_targets": total_no_targets,
+        "num_items": total_items,
+    }
+
+    log_test_to_wandb(aggregated_res)
+
 
 def evaluate(config: Any):
 
