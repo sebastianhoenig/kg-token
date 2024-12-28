cb8fd8de31d4adea318dd63716023367c8b3ab91
Sebastian Hönig <hoenigsebastian@gmail.com>
2024-12-28 11:35:01 +0100

    New Prompt

--- src/models/gnn_llm.py
+++ src/models/gnn_llm.py
@@ -165,7 +165,7 @@ class GraphTokenGPT(nn.Module):
             query_tokens = self.tokenizer(question, add_special_tokens=False)["input_ids"]
             BOS_TOKEN = self.tokenizer.bos_token_id
             PAD_TOKEN = self.tokenizer.eos_token_id  # TODO CHANGE AS WELL WHEN NO LONGER GPT2
-            max_tokens = 35
+            max_tokens = 50
             input_token = np.array([BOS_TOKEN] + query_tokens)
             orig_len = len(query_tokens)
             input_token = np.pad(input_token, [[0, max_tokens - orig_len - 1]], constant_values=PAD_TOKEN)
